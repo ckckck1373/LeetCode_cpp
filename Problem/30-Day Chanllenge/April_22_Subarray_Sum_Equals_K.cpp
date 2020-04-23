a50c6722a42595786b3ccafab367bf675a0104f6
Chun Yeh <ckckck942039@yahoo.com.tw>
2020-04-23 19:59:52 +0800

    Update April_22_Subarray_Sum_Equals_K.cpp

--- Problem/30-Day Chanllenge/April_22_Subarray_Sum_Equals_K.cpp
+++ Problem/30-Day Chanllenge/April_22_Subarray_Sum_Equals_K.cpp
@@ -27,8 +27,41 @@ public:
     }
 };
 
-// version 2  : hash table、unordered map
-//記錄每一個從頭開始算的sum，如果剛好有特定可以砍掉的頭，即是result++
+// version 2(use iter and .second)
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int sum=0;
+        unordered_map<int, int> sum_map;
+        sum_map.emplace(0, 1);
+        unordered_map<int, int>::iterator iter, iter1;
+
+        int num_subarray=0;
+        for(int i=0;i<nums.size();i++){
+            sum += nums[i];
+            iter = sum_map.find(sum-k);
+            iter1 = sum_map.find(sum);
+
+
+            if(iter!=sum_map.end()){
+                num_subarray += iter->second;
+            }
+            
+            if(iter1==sum_map.end()){
+                sum_map.emplace(sum, 1);
+            }else{
+                iter1->second++;
+            }
+        }
+
+        return num_subarray;
+    }
+};
+
+
+// version 3  : hash table、unordered map
+//記錄每一個從頭開始算的sum，如果剛好有特定可以砍掉的頭，即是result++(這個map沒有在開頭塞0，所以要判斷sum是否就等於k)
 
 
 class Solution {
@@ -43,10 +76,10 @@ public:
             if(cur_sum==k){
                 ans++;
             }
-            if(m.find(cur_sum-k)!=m.end()){
+            if(m.find(cur_sum-k)!=m.end()){ // use find can avoid using iter
                 ans+=m[cur_sum-k];
             }
-            m[cur_sum]++;
+            m[cur_sum]++; // unordermap can use [] find the key and directly use it even if the key haven't been the dictionary()
         }
         return ans;
     }
