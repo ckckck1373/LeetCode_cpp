61b26f0f8584b0c000d33755e6a1cb4cc65f9609
Chun Yeh <ckckck942039@yahoo.com.tw>
2020-04-25 13:08:39 +0800

    Update April_19_Search_In_Rotated_Sorted_Array.cpp

--- Problem/30-Day Chanllenge/April_19_Search_In_Rotated_Sorted_Array.cpp
+++ Problem/30-Day Chanllenge/April_19_Search_In_Rotated_Sorted_Array.cpp
@@ -12,6 +12,7 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         vector<int>::iterator iter;
+        iter=find(nums.begin(), nums.end(), target);
         if(iter==nums.end())return -1;
         else return iter - nums.begin();
              
